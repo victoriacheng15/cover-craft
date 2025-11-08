df0a2c57a06bd842d28bb5db6e91cdd1e6c35de7
Victoria Cheng <victoria.cheng15@gmail.com>
2025-11-08 01:43:41 +0000

    add moogose, metrics for generate and download

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,6 +1,13 @@
 import { downloadImage, generateCoverImage, healthCheck } from "./api";
 import type { ApiError, HealthCheckResponse, ImageParams } from "./types";
-import { cn } from "./utils";
+import { cn, sendDownloadMetric, sendGenericMetric } from "./utils";
 
-export { cn, downloadImage, generateCoverImage, healthCheck };
+export {
+  cn,
+  downloadImage,
+  generateCoverImage,
+  healthCheck,
+  sendDownloadMetric,
+  sendGenericMetric,
+};
 export type { ApiError, HealthCheckResponse, ImageParams };
