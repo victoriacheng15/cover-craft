3dc72dffd2e59c3e973ddc41a28f7e60306c1b23
Victoria Cheng <victoria.cheng15@gmail.com>
2026-04-09 03:22:41 +0000

    feat(infra): migrate to flex consumption and node 22

--- tofu/modules/function_app/main.tf
+++ tofu/modules/function_app/main.tf
@@ -10,11 +10,19 @@ variable "app_name" {
   type = string
 }
 
+variable "storage_account_id" {
+  type = string
+}
+
 variable "storage_account_name" {
   type = string
 }
 
 variable "storage_account_access_key" {
+  type = string
+}
+
+variable "storage_container_endpoint" {
   type = string
 }
 
@@ -30,27 +38,44 @@ variable "app_insights_instrumentation_key" {
   type = string
 }
 
-# Consumption Plan (Serverless / Pay-as-you-go)
-resource "azurerm_service_plan" "consumption_plan" {
+# 1. Storage Container for Flex Consumption Deployment
+resource "azurerm_storage_container" "deploy" {
+  name                 = "app-package"
+  storage_account_id   = var.storage_account_id
+  container_access_type = "private"
+}
+
+# 2. Flex Consumption Plan (FC1)
+resource "azurerm_service_plan" "flex_plan" {
   name                = "asp-${var.app_name}-api"
   resource_group_name = var.resource_group_name
   location            = var.location
   os_type             = "Linux"
-  sku_name            = "Y1" # Y1 is the Consumption SKU
+  sku_name            = "FC1"
 }
 
-resource "azurerm_linux_function_app" "api" {
+# 3. Flex Consumption Function App
+# Using the stable top-level attributes for azurerm v4.x
+resource "azurerm_function_app_flex_consumption" "api" {
   name                = var.app_name
   resource_group_name = var.resource_group_name
   location            = var.location
+  service_plan_id     = azurerm_service_plan.flex_plan.id
 
-  storage_account_name       = var.storage_account_name
-  storage_account_access_key = var.storage_account_access_key
-  service_plan_id            = azurerm_service_plan.consumption_plan.id
+  instance_memory_in_mb = 2048
+
+  runtime_name    = "node"
+  runtime_version = "22"
+
+  storage_container_type           = "blobContainer"
+  storage_container_endpoint       = "${var.storage_container_endpoint}${azurerm_storage_container.deploy.name}"
+  storage_authentication_type      = "StorageAccountConnectionString"
+  storage_access_key               = var.storage_account_access_key
 
   site_config {
-    application_stack {
-      node_version = "24"
+    app_service_logs {
+      disk_quota_mb         = 35
+      retention_period_days = 7
     }
   }
 
@@ -58,6 +83,6 @@ resource "azurerm_linux_function_app" "api" {
     "MONGODB_URI"                           = var.mongodb_uri
     "APPINSIGHTS_INSTRUMENTATIONKEY"        = var.app_insights_instrumentation_key
     "APPLICATIONINSIGHTS_CONNECTION_STRING" = var.app_insights_connection_string
-    "WEBSITE_RUN_FROM_PACKAGE"              = "1"
+    "SCM_DO_BUILD_DURING_DEPLOYMENT"        = "false"
   }
 }
