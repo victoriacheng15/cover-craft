631e2480c2985eeca71dbe54afe01fcd546a96f5
Hsiu-Chia Cheng <victoria.cheng15@gmail.com>
2026-06-19 10:07:37 -0700

    refactor(frontend): consolidate form and ui components

--- frontend/src/components/form/index.ts
+++ frontend/src/components/form/index.ts
@@ -1,7 +1,4 @@
 export { BatchFormControls } from "./BatchFormControls";
-export { ColorContrastMessage } from "./ColorContrastMessage";
-export { ColorControls } from "./ColorControls";
 export { CoverForm } from "./CoverForm";
 export { CoverFormControls } from "./CoverFormControls";
 export { CoverPreviewDisplay } from "./CoverPreviewDisplay";
-export { FormField } from "./FormField";
